908459b2a8fbca68545cdb0cd79ef3647f73dc2c
salik-a <salika@mef.edu.tr>
2021-08-30 19:58:36 +0300

    react native project created

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+const App= () => {
+ 
+
+  return (
+    <View >
+      <Text>merhaba</Text>
+    </View>
+  );
+};
+
+
+export default App;
